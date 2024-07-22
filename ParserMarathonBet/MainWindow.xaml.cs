f86535604859b26a471e96926c69cd2f06a4c122
Abelint <abelint19@gmail.com>
2024-07-22 18:03:52 +0700

    добавлен subject

--- ParserMarathonBet/MainWindow.xaml.cs
+++ ParserMarathonBet/MainWindow.xaml.cs
@@ -2,26 +2,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Xml;
 using HtmlAgilityPack;
 
 namespace ParserMarathonBet
 {
     public partial class MainWindow : Window
     {
-        private ObservableCollection<GroupData> groups = new ObservableCollection<GroupData>();
+     
+
+        private ObservableCollection<SubjectData> subjects = new ObservableCollection<SubjectData>();
 
         public MainWindow()
         {
             InitializeComponent();
-            GroupsListBox.ItemsSource = groups;
+            // GroupsListBox.ItemsSource = groups;
+            SubjectsListBox.ItemsSource = subjects;
         }
 
         private async void LoadButton_Click(object sender, RoutedEventArgs e)
         {
+            subjects.Clear();
             string url = UrlTextBox.Text;
             await ParsePageAsync(url);
         }
@@ -60,28 +68,70 @@ namespace ParserMarathonBet
                     return;  // Return early since we're parsing the new URL
                 }
 
-                // Clear the list box and add new items
-                var nodes = htmlDoc.DocumentNode.SelectNodes("//div[contains(@class, 'category-container')]");
-                if (nodes != null)
+                // Найти контейнер с data-id="container_EVENTS"
+                var container = htmlDoc.DocumentNode
+                    .SelectSingleNode("//div[@data-id='container_EVENTS']");
+
+                if (container != null)
                 {
-                   
-                    int count = 0;
-                    foreach (var node in nodes)
+                    // Найти все div внутри контейнера с data-sport-treeid="1372932"
+                    var divBlocks = container
+                        .SelectNodes(".//div[contains(@class, 'sport-category-container')]");
+
+                    if (divBlocks != null)
                     {
-                       // var someData = node.InnerText.Trim().Replace("\n", "");
-                        var someData = node.InnerHtml.Trim().Replace("\n", "");
-                       
-                        count++;
-                       
-                        if (count > 1) Show(someData);
-                       
+                        foreach (var div in divBlocks)
+                        {
+                            var header = div.SelectSingleNode(".//div[contains(@class, 'sport-category-header')]");
+                            SubjectData subject = new SubjectData();
+                            if (header != null)
+                            {
+                                // Найти элемент с классом sport-category-label и получить его текст
+                                var label = header.SelectSingleNode(".//a[contains(@class, 'sport-category-label')]");
+                                if (label != null)
+                                {
+                                   string categoryName = label.InnerText.Trim();
+                                   subject.SubjectName = categoryName;
+                                }
+                            }
+
+                            // Clear the list box and add new items
+                            var nodes = htmlDoc.DocumentNode.SelectNodes("//div[contains(@class, 'category-container')]");
+                            if (nodes != null)
+                            {
+                                subject.Groups = new List<GroupData>();
+                                int count = 0;
+                                foreach (var node in nodes)
+                                {
+                                    // var someData = node.InnerText.Trim().Replace("\n", "");
+                                    var someData = node.InnerHtml.Trim().Replace("\n", "");
+
+                                    count++;
+                                    var parser = new HtmlParser2();
+                                    subject.Groups = parser.ParseHtml(html);   
+
+                                }
+
+                            }
+                            else
+                            {
+                                MessageBox.Show("Не удалось найти элементы по заданному XPath.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                            }
+                            subjects.Add(subject);
+                        }
+
                     }
-                    
+                    else
+                    {
+                        Console.WriteLine("No div blocks found with data-sport-treeid='1372932'");
+                    }
                 }
                 else
                 {
-                    MessageBox.Show("Не удалось найти элементы по заданному XPath.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    Console.WriteLine("No container found with data-id='container_EVENTS'");
                 }
+
+                
             }
             catch (Exception ex)
             {
@@ -89,27 +139,62 @@ namespace ParserMarathonBet
             }
         }
 
-        private void Show(string html)
-        {
+       
 
-            //var parser = new HtmlParser();
-            var parser = new HtmlParser2();
-            foreach (var p in parser.ParseHtml(html))
+     
+
+        private void SubjectsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (SubjectsListBox.SelectedItems.Count > 0)
             {
-                groups.Add(p);
+                var selectedSubjects = SubjectsListBox.SelectedItems.Cast<SubjectData>().ToList();
+                var groups = selectedSubjects.SelectMany(s => s.Groups).Distinct().ToList();
+                GroupsListBox.ItemsSource = groups;
             }
-            
-
-            
-        }
-
-        private void GroupsListBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-            if (GroupsListBox.SelectedItem is GroupData selectedGroup)
+            else
             {
-                EventsDataGrid.ItemsSource = selectedGroup.Events;
-                Dictionary<string, string> tempDict = selectedGroup.Events[0].EventColumns;
+                GroupsListBox.ItemsSource = null;
+                EventsDataGrid.ItemsSource = null;
             }
         }
+
+        private void GroupsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (GroupsListBox.SelectedItems.Count > 0)
+            {
+                var selectedGroups = GroupsListBox.SelectedItems.Cast<GroupData>().ToList();
+                var events = selectedGroups.SelectMany(g => g.Events).Distinct().ToList();
+                EventsDataGrid.ItemsSource = events;
+                UpdateEventColumns(events);
+            }
+            else
+            {
+                EventsDataGrid.ItemsSource = null;
+            }
+        }
+
+        private void UpdateEventColumns(List<EventData> events)
+        {
+            EventsDataGrid.Columns.Clear();
+
+            EventsDataGrid.Columns.Add(new DataGridTextColumn { Header = "EventName", Binding = new Binding("EventName") });
+            EventsDataGrid.Columns.Add(new DataGridTextColumn { Header = "Team1", Binding = new Binding("Team1") });
+            EventsDataGrid.Columns.Add(new DataGridTextColumn { Header = "Team2", Binding = new Binding("Team2") });
+
+            if (events.Any())
+            {
+                var allKeys = events.SelectMany(e => e.EventColumns.Keys).Distinct().ToList();
+                foreach (var key in allKeys)
+                {
+                    EventsDataGrid.Columns.Add(new DataGridTextColumn
+                    {
+                        Header = key,
+                        Binding = new Binding($"EventColumns[{key}]")
+                    });
+                }
+            }
+        }
+
+      
     }
 }
---
+++ ParserMarathonBet/SubjectData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserMarathonBet
+{
+    public class SubjectData
+    {
+        public string SubjectName { get; set; }
+        public List<GroupData> Groups { get; set; }
+    }
+}
